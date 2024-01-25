c81de402b70b7cceb80c0a45f695c1627fbdd062
Sugam Goel <sg028.online@gmail.com>
2024-01-25 23:14:40 +0530

    feat: add GitHub icon and linkedin url

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import "../../App.css"; // Import the CSS file
+import githubIcon from "./github.png";
 
 const Navbar = () => {
   return (
@@ -7,15 +8,21 @@ const Navbar = () => {
         className="d-flex position-absolute top-0 end-0 z-2 justify-content-end"
         id="socials"
       >
-        <a className="hover-effect p-2" href="">
+        <a
+          className="hover-effect p-2"
+          href="https://github.com/Sugamgoel28/EaseDraw"
+        >
           <img
-            src="./github.png"
+            src={githubIcon}
             alt="GitHub Logo"
             width="40"
             className="d-inline-block align-text-top"
           />
         </a>
-        <a className="hover-effect p-2" href="#">
+        <a
+          className="hover-effect p-2"
+          href="https://www.linkedin.com/in/sugam-goel-india/"
+        >
           <img
             src="https://img.icons8.com/ios-filled/50/linkedin.png"
             alt="LinkedIn Logo"
