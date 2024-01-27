240b823b08bfdde435eaf4a3b7e45f3671670137
Sugam Goel <sg028.online@gmail.com>
2024-01-27 20:30:49 +0530

    feat: reduced toolbar icons

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -1,5 +1,3 @@
-import UndoIcon from "@mui/icons-material/Undo";
-import RedoIcon from "@mui/icons-material/Redo";
 import RectangleOutlinedIcon from "@mui/icons-material/RectangleOutlined";
 import DeleteIcon from "@mui/icons-material/Delete";
 import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
@@ -13,98 +11,8 @@ const Toolbar = ({
   setTool,
   color,
   setColor,
-  elements,
-  setElements,
-  history,
-  setHistory,
-  canvasRef,
+  handleClearCanvas,
 }) => {
-  const handleClearCanvas = () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    ctx.fillRect = "white";
-    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-    setElements([]);
-  };
-
-  const undo = () => {
-    setHistory((prevHistory) => [
-      ...prevHistory,
-      elements[elements.length - 1],
-    ]);
-    if (elements.length === 1) handleClearCanvas();
-    setElements((prevElements) =>
-      prevElements.slice(0, prevElements.length - 1)
-    );
-  };
-
-  const redo = () => {
-    setElements((prevElements) => [
-      ...prevElements,
-      history[history.length - 1],
-    ]);
-    setHistory((prevHistory) => prevHistory.slice(0, prevHistory.length - 1));
-  };
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      // Check if the Ctrl (Cmd on Mac) key is pressed
-      const isCtrlPressed = event.ctrlKey || event.metaKey;
-
-      // Check for keyboard shortcuts
-      if (isCtrlPressed) {
-        switch (event.key.toLowerCase()) {
-          case "z":
-            // Undo shortcut (Ctrl + Z)
-            event.preventDefault();
-            undo();
-            break;
-          case "y":
-            // Redo shortcut (Ctrl + Y)
-            event.preventDefault();
-            redo();
-            break;
-          case "d":
-            // Clear canvas shortcut (Ctrl + D)
-            event.preventDefault();
-            handleClearCanvas();
-            break;
-
-          default:
-            break;
-        }
-      } else {
-        // Check for tool shortcuts without Ctrl
-        switch (event.key.toLowerCase()) {
-          case "p":
-            // Set pencil tool (P key)
-            setTool("pencil");
-            break;
-          case "l":
-            // Set line tool (L key)
-            setTool("line");
-            break;
-          case "r":
-            // Set rectangle tool (R key)
-            setTool("rect");
-            break;
-          case "c":
-            setTool("circle");
-            break;
-
-          default:
-            break;
-        }
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [undo, redo, handleClearCanvas, setTool]);
-
   return (
     <div
       className="btn-group btn-group-toggle btn-group-vertical d-flex flex-column justify-content-center align-items-center z-2"
@@ -189,20 +97,6 @@ const Toolbar = ({
           onChange={(e) => setColor(e.target.value)}
         />
       </label>
-      <button
-        className="btn btn-light border text-primary py-4"
-        disabled={elements.length === 0}
-        onClick={() => undo()}
-      >
-        <UndoIcon color="primary" />
-      </button>
-      <button
-        className="btn btn-light border py-4"
-        disabled={history.length < 1}
-        onClick={() => redo()}
-      >
-        <RedoIcon color="primary" />
-      </button>
       <button className="btn btn-danger p-3" onClick={handleClearCanvas}>
         <DeleteIcon fontSize="large" />
       </button>
--- src/components/Whiteboard/index.jsx
+++ src/components/Whiteboard/index.jsx
@@ -4,6 +4,8 @@ import Toolbar from "../Toolbar";
 import Shortcuts from "../KeyboardShortcuts/Shortcuts";
 import Placeholder from "../Placeholder/Placeholder";
 import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+import UndoIcon from "@mui/icons-material/Undo";
+import RedoIcon from "@mui/icons-material/Redo";
 
 const roughGenerator = rough.generator();
 
@@ -19,6 +21,171 @@ const WhiteBoard = ({ isToolbarVisible }) => {
 
   const [isDrawing, setIsDrawing] = useState(false);
   const [hasUserClicked, setHasUserClicked] = useState(false);
+
+  const undo = () => {
+    setHistory((prevHistory) => [
+      ...prevHistory,
+      elements[elements.length - 1],
+    ]);
+    if (elements.length === 1) handleClearCanvas();
+    setElements((prevElements) =>
+      prevElements.slice(0, prevElements.length - 1)
+    );
+  };
+
+  const redo = () => {
+    setElements((prevElements) => [
+      ...prevElements,
+      history[history.length - 1],
+    ]);
+    setHistory((prevHistory) => prevHistory.slice(0, prevHistory.length - 1));
+  };
+
+  const handleMouseMove = (e) => {
+    const { offsetX, offsetY } = e.nativeEvent;
+    if (isDrawing) {
+      if (tool === "pencil") {
+        const { path } = elements[elements.length - 1];
+        const newPath = [...path, [offsetX, offsetY]];
+
+        setElements((prevElements) =>
+          prevElements.map((element, index) => {
+            if (index === elements.length - 1) {
+              return {
+                ...element,
+                path: newPath,
+              };
+            } else {
+              return element;
+            }
+          })
+        );
+      } else if (tool === "line") {
+        setElements((prevElements) =>
+          prevElements.map((element, index) => {
+            if (index === elements.length - 1) {
+              return {
+                ...element,
+                width: offsetX,
+                height: offsetY,
+              };
+            } else {
+              return element;
+            }
+          })
+        );
+      } else if (tool === "rect") {
+        setElements((prevElements) =>
+          prevElements.map((element, index) => {
+            if (index === elements.length - 1) {
+              return {
+                ...element,
+                width: offsetX - element.offsetX,
+                height: offsetY - element.offsetY,
+              };
+            } else {
+              return element;
+            }
+          })
+        );
+      } else if (tool === "circle") {
+        setElements((prevElements) =>
+          prevElements.map((element, index) => {
+            if (index === elements.length - 1) {
+              const a = Math.abs(offsetX - element.offsetX);
+              const b = Math.abs(offsetY - element.offsetY);
+              const diameter = Math.sqrt(a * a + b * b) * 2;
+              return {
+                ...element,
+                diameter: diameter,
+              };
+            } else {
+              return element;
+            }
+          })
+        );
+      }
+    }
+  };
+
+  const handleMouseUp = (e) => {
+    setIsDrawing(false);
+  };
+  
+  const handleMouseDown = (e) => {
+    const { offsetX, offsetY } = e.nativeEvent;
+
+    if (tool === "pencil") {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: "pencil",
+          offsetX,
+          offsetY,
+          path: [[offsetX, offsetY]],
+          stroke: color,
+        },
+      ]);
+    } else if (tool === "line") {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: "line",
+          offsetX,
+          offsetY,
+          width: offsetX,
+          height: offsetY,
+          stroke: color,
+        },
+      ]);
+    } else if (tool === "rect") {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: "rect",
+          offsetX,
+          offsetY,
+          width: 0,
+          height: 0,
+          stroke: color,
+        },
+      ]);
+    } else if (tool === "circle") {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: "circle",
+          offsetX,
+          offsetY,
+          diameter: 0,
+          stroke: color,
+        },
+      ]);
+    }
+    setIsDrawing(true);
+  };
+
+  const handleDownload = () => {
+    const canvas = canvasRef.current;
+
+    // Create a temporary link element
+    const link = document.createElement("a");
+    link.href = canvas.toDataURL("image/png");
+    link.download = "whiteboard.png";
+
+    // Trigger a click on the link to start the download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const handleClearCanvas = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.fillRect = "white";
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    setElements([]);
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -48,6 +215,66 @@ const WhiteBoard = ({ isToolbarVisible }) => {
   useEffect(() => {
     ctxRef.current.strokeStyle = color;
   }, [color]);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      // Check if the Ctrl (Cmd on Mac) key is pressed
+      const isCtrlPressed = event.ctrlKey || event.metaKey;
+
+      // Check for keyboard shortcuts
+      if (isCtrlPressed) {
+        switch (event.key.toLowerCase()) {
+          case "z":
+            // Undo shortcut (Ctrl + Z)
+            event.preventDefault();
+            undo();
+            break;
+          case "y":
+            // Redo shortcut (Ctrl + Y)
+            event.preventDefault();
+            redo();
+            break;
+          case "d":
+            // Clear canvas shortcut (Ctrl + D)
+            event.preventDefault();
+            handleClearCanvas();
+            break;
+
+          default:
+            break;
+        }
+      } else {
+        // Check for tool shortcuts without Ctrl
+        switch (event.key.toLowerCase()) {
+          case "p":
+            // Set pencil tool (P key)
+            setTool("pencil");
+            break;
+          case "l":
+            // Set line tool (L key)
+            setTool("line");
+            break;
+          case "r":
+            // Set rectangle tool (R key)
+            setTool("rect");
+            break;
+          case "c":
+            setTool("circle");
+            break;
+
+          default:
+            break;
+        }
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [undo, redo, handleClearCanvas, setTool]);
+
 
   useLayoutEffect(() => {
     if (canvasRef) {
@@ -114,143 +341,6 @@ const WhiteBoard = ({ isToolbarVisible }) => {
     }
   }, [elements]);
 
-  const handleMouseDown = (e) => {
-    const { offsetX, offsetY } = e.nativeEvent;
-
-    if (tool === "pencil") {
-      setElements((prevElements) => [
-        ...prevElements,
-        {
-          type: "pencil",
-          offsetX,
-          offsetY,
-          path: [[offsetX, offsetY]],
-          stroke: color,
-        },
-      ]);
-    } else if (tool === "line") {
-      setElements((prevElements) => [
-        ...prevElements,
-        {
-          type: "line",
-          offsetX,
-          offsetY,
-          width: offsetX,
-          height: offsetY,
-          stroke: color,
-        },
-      ]);
-    } else if (tool === "rect") {
-      setElements((prevElements) => [
-        ...prevElements,
-        {
-          type: "rect",
-          offsetX,
-          offsetY,
-          width: 0,
-          height: 0,
-          stroke: color,
-        },
-      ]);
-    } else if (tool === "circle") {
-      setElements((prevElements) => [
-        ...prevElements,
-        {
-          type: "circle",
-          offsetX,
-          offsetY,
-          diameter: 0,
-          stroke: color,
-        },
-      ]);
-    }
-    setIsDrawing(true);
-  };
-
-  const handleMouseMove = (e) => {
-    const { offsetX, offsetY } = e.nativeEvent;
-    if (isDrawing) {
-      if (tool === "pencil") {
-        const { path } = elements[elements.length - 1];
-        const newPath = [...path, [offsetX, offsetY]];
-
-        setElements((prevElements) =>
-          prevElements.map((element, index) => {
-            if (index === elements.length - 1) {
-              return {
-                ...element,
-                path: newPath,
-              };
-            } else {
-              return element;
-            }
-          })
-        );
-      } else if (tool === "line") {
-        setElements((prevElements) =>
-          prevElements.map((element, index) => {
-            if (index === elements.length - 1) {
-              return {
-                ...element,
-                width: offsetX,
-                height: offsetY,
-              };
-            } else {
-              return element;
-            }
-          })
-        );
-      } else if (tool === "rect") {
-        setElements((prevElements) =>
-          prevElements.map((element, index) => {
-            if (index === elements.length - 1) {
-              return {
-                ...element,
-                width: offsetX - element.offsetX,
-                height: offsetY - element.offsetY,
-              };
-            } else {
-              return element;
-            }
-          })
-        );
-      } else if (tool === "circle") {
-        setElements((prevElements) =>
-          prevElements.map((element, index) => {
-            if (index === elements.length - 1) {
-              const a = Math.abs(offsetX - element.offsetX);
-              const b = Math.abs(offsetY - element.offsetY);
-              const diameter = Math.sqrt(a * a + b * b)*2;
-              return {
-                ...element,
-                diameter: diameter,
-              };
-            } else {
-              return element;
-            }
-          })
-        );
-      }}
-  };
-
-  const handleMouseUp = (e) => {
-    setIsDrawing(false);
-  };
-
-  const handleDownload = () => {
-    const canvas = canvasRef.current;
-
-    // Create a temporary link element
-    const link = document.createElement("a");
-    link.href = canvas.toDataURL("image/png");
-    link.download = "whiteboard.png";
-
-    // Trigger a click on the link to start the download
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   return (
     <div
       onMouseDown={handleMouseDown}
@@ -268,15 +358,27 @@ const WhiteBoard = ({ isToolbarVisible }) => {
             setTool={setTool}
             color={color}
             setColor={setColor}
-            elements={elements}
-            setElements={setElements}
-            history={history}
-            setHistory={setHistory}
-            canvasRef={canvasRef}
+            handleClearCanvas={handleClearCanvas}
           />
         )}
       </div>
       <div className="position-absolute bottom-0 end-0 gap-3 m-4 d-flex align-items-end">
+        <div>
+          <button
+            className="btn btn-light border p-2 rounded-end-0"
+            disabled={elements.length === 0}
+            onClick={() => undo()}
+          >
+            <UndoIcon />
+          </button>
+          <button
+            className="btn btn-light border p-2 border-start-0 rounded-start-0 "
+            disabled={history.length < 1}
+            onClick={() => redo()}
+          >
+            <RedoIcon />
+          </button>
+        </div>
         <button className="btn btn-light border p-2" onClick={handleDownload}>
           <FileDownloadOutlinedIcon />
         </button>
